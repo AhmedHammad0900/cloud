58df4a44e5753929069ad3344632736ec25e7d25
AhmedHammad0900 <81931290+AhmedHammad0900@users.noreply.github.com>
2024-07-18 17:19:34 +0300

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ Future<dynamic> main(final context) async {
           teamId: parsing.teamId,
           roles: theFinalRoles,
           email: parsing.userEmail,
-          url: "cloud.appwrite.io"
+          url: "https://cloud.appwrite.io"
       );
       context.log(result.userEmail);
     }  catch (e) {
