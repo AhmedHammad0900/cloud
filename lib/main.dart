83adc4b80072a169bc162f812161ecef6b883bf9
AhmedHammad0900 <81931290+AhmedHammad0900@users.noreply.github.com>
2024-07-18 18:54:24 +0300

    eraaa

--- lib/main.dart
+++ lib/main.dart
@@ -4,47 +4,49 @@ import 'dart:io';
 import 'package:dart_appwrite/dart_appwrite.dart';
 import 'package:dart_appwrite/models.dart';
 
-// This is your Appwrite function
-// It's executed each time we get a request
+
+late Teams teams ;
+late Users users ;
+List<String> theFinalRoles = [] ;
+
 Future<dynamic> main(final context) async {
   final client = Client()
      .setEndpoint('https://cloud.appwrite.io/v1')
      .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
      .setKey(Platform.environment['APPWRITE_API_KEY']);
-  Teams teams = Teams(client);
+  teams = Teams(client);
+  users = Users(client);
   ParseData parsing = ParseData.fromJson(json.decode(context.req.body));
   // The `req` object contains the request data
   if (context.req.method == 'POST') {
-    List<String> theFinalRoles = [] ;
     for ( int i = 0 ; i < parsing.roles.length ; i ++ ) {
       theFinalRoles.add(parsing.roles[i]) ;
     }
-    context.log(theFinalRoles);
-    context.log(theFinalRoles.runtimeType);
     try {
       Membership result = await teams.createMembership(
           teamId: parsing.teamId,
           roles: theFinalRoles,
           email: parsing.userEmail,
-          url: "https://cloud.appwrite.io"
+          url: "https://cloud.appwrite.io",
+          name: "Dr.Dentist"
       );
       context.log(result.userEmail);
-    }  catch (e) {
-      context.error("paresed but I am here");
-      context.error(e);
+    }  on AppwriteException catch (e) {
+      if (e.code == 409 ) {
+        updateUser( parsing.teamId, parsing.userEmail.substring(0, parsing.userEmail.indexOf("@")) );
+        context.log( "Updated ${parsing.userEmail}" );
+      }
 
     }
 
     return context.res.send('Hello, World!');
   }
 
-    return context.res.json({
-    'motto': 'Build like a team of hundreds_',
-    'learn': 'https://appwrite.io/docs',
-    'connect': 'https://appwrite.io/discord',
-    'getInspired': 'https://builtwith.appwrite.io',
-  });
+
+
+
 }
+
 
 class ParseData {
   final String teamId;
@@ -67,4 +69,11 @@ class ParseData {
         adminDocumentId: json['myId'],
         newUserOrPlus: json['newUserOrPlus']);
   }
+}
+
+Future<bool> updateUser(String theTeamId, String userEmail) async{
+  UserList theUser =  await users.list(search: userEmail);
+  MembershipList membershipList = await teams.listMemberships(teamId: theTeamId, search: theUser.users[0].$id) ;
+  Membership membership = await teams.updateMembershipRoles(teamId: theTeamId, membershipId: membershipList.memberships[0].$id, roles: theFinalRoles) ;
+  return membership.confirm ;
 }
